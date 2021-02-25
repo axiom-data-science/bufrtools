0ddc11ad92070297fe1b9661c7d88ea5281476e2
Kyle Wilcox <kyle@axiomdatascience.com>
2021-02-24 22:52:44 -0500

    Replace netCDF as the base data source with pandas DataFrames
    To allow for in-pipeline processing without converting to a netCDF file
    first I made changes to be able to pass in a path to a CSV or Parquet
    file in addition to a netCDF file.
    
    The CSV and Parquet files have no metadata so the uuid and ptt attributes
    were added as parameters. If a netCDF file is the data source those
    attributes are extracted from the file and the parameters are ignored.
    
    Also removed an activate script from root dir

--- bufrtools/util/parse.py
+++ bufrtools/util/parse.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 """Module for basic and general parsing functions."""
+from pathlib import Path
+
+import pandas as pd
+from pocean.dsg import *  # noqa Import required for CFDataset
+from pocean.cf import CFDataset
 
 
 def parse_ref(fxy) -> tuple:
@@ -9,3 +14,59 @@ def parse_ref(fxy) -> tuple:
     x = int(fxy[1:3])
     y = int(fxy[3:6])
     return f, x, y
+
+
+def load_csv(ipt: Path) -> pd.DataFrame:
+    df = pd.read_csv(ipt)
+    df['time'] = pd.to_datetime(df.time)
+    return (
+        df,
+        {}
+    )
+
+
+def load_parquet(ipt: Path) -> pd.DataFrame:
+    return (
+        pd.read_parquet(ipt),
+        {}
+    )
+
+
+def load_netcdf(ipt: Path) -> pd.DataFrame:
+    ds = CFDataset.load(str(ipt))
+    axes = dict(
+        t='time',
+        z='z',
+        x='lon',
+        y='lat',
+        profile='profile',
+        trajectory='trajectory'
+    )
+
+    meta = ds.meta()['attributes']
+    valid_meta = {
+        'uuid': meta.get('uuid')['data'],
+        'ptt': meta.get('ptt', '')['data']
+    }
+    return (
+        ds.to_dataframe(axes=axes, clean_cols=False, clean_rows=False),
+        valid_meta
+    )
+
+
+def parse_input_to_dataframe(ipt: Path) -> pd.DataFrame:
+    if isinstance(ipt, pd.DataFrame):
+        return ipt
+    else:
+        # Try to load as a pandas dataset file
+        loaders = [
+            load_csv,
+            load_parquet,
+            load_netcdf
+        ]
+        for load_func in loaders:
+            try:
+                return load_func(ipt)
+            except BaseException:
+                pass
+        raise ValueError(f"Could not load {ipt} as a dataset")
